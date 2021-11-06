41a9fc59779a6347c064d99319d109b92459e633
juanvaleriomayta <jvaleriom@vallegrande.edu.pe>
2021-11-06 17:58:46 -0500

    avances del portafolio

--- portafolio-backend/src/main/java/com/portafolio/portafoliobackend/PortafolioBackendApplication.java
+++ portafolio-backend/src/main/java/com/portafolio/portafoliobackend/PortafolioBackendApplication.java
@@ -2,9 +2,15 @@ package com.portafolio.portafoliobackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PortafolioBackendApplication {
+public class PortafolioBackendApplication extends SpringBootServletInitializer {
+
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(PortafolioBackendApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PortafolioBackendApplication.class, args);
